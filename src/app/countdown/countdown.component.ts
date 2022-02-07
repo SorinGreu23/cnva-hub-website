99d116be4d50b4ac7440aa76f9d64b9ffcc43244
Sorin Greu <greusorin2003@gmail.com>
2022-02-07 08:44:38 +0200

    Removed the counter and added the sponsors

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -7,50 +7,50 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./countdown.component.scss'],
 })
 export class CountdownComponent implements OnInit, OnDestroy {
-  private subscription: Subscription | any;
+  // private subscription: Subscription | any;
 
-  public dateNow = new Date();
-  public dDay = new Date('Feb 06 2022 23:59:59');
-  msInSecond = 1000;
-  hoursInDay = 24;
-  minInHour = 60;
-  secInMin = 60;
+  // public dateNow = new Date();
+  // public dDay = new Date('Feb 06 2022 23:59:59');
+  // msInSecond = 1000;
+  // hoursInDay = 24;
+  // minInHour = 60;
+  // secInMin = 60;
 
-  public timeDifference: any;
-  public secondsToDday: any;
-  public minutesToDday: any;
-  public hoursToDday: any;
-  public daysToDday: any;
+  // public timeDifference: any;
+  // public secondsToDday: any;
+  // public minutesToDday: any;
+  // public hoursToDday: any;
+  // public daysToDday: any;
 
-  private getTimeDifference() {
-    this.timeDifference = this.dDay.getTime() - new Date().getTime();
-    this.allocateTimeUnits(this.timeDifference);
-  }
+  // private getTimeDifference() {
+  //   this.timeDifference = this.dDay.getTime() - new Date().getTime();
+  //   this.allocateTimeUnits(this.timeDifference);
+  // }
 
-  private allocateTimeUnits(timeDifference: number) {
-    this.secondsToDday = Math.floor(
-      (this.timeDifference / this.msInSecond) % this.secInMin
-    );
-    this.minutesToDday = Math.floor(
-      (timeDifference / (this.msInSecond * this.minInHour)) % this.secInMin
-    );
-    this.hoursToDday = Math.floor(
-      (timeDifference / (this.msInSecond * this.minInHour * this.secInMin)) %
-        this.hoursInDay
-    );
-    this.daysToDday = Math.floor(
-      timeDifference /
-        (this.msInSecond * this.minInHour * this.secInMin * this.hoursInDay)
-    );
-  }
+  // private allocateTimeUnits(timeDifference: number) {
+  //   this.secondsToDday = Math.floor(
+  //     (this.timeDifference / this.msInSecond) % this.secInMin
+  //   );
+  //   this.minutesToDday = Math.floor(
+  //     (timeDifference / (this.msInSecond * this.minInHour)) % this.secInMin
+  //   );
+  //   this.hoursToDday = Math.floor(
+  //     (timeDifference / (this.msInSecond * this.minInHour * this.secInMin)) %
+  //       this.hoursInDay
+  //   );
+  //   this.daysToDday = Math.floor(
+  //     timeDifference /
+  //       (this.msInSecond * this.minInHour * this.secInMin * this.hoursInDay)
+  //   );
+  // }
 
   ngOnInit(): void {
-    this.subscription = interval(1000).subscribe((x) => {
-      this.getTimeDifference();
-    });
+    // this.subscription = interval(1000).subscribe((x) => {
+    //   this.getTimeDifference();
+    // });
   }
 
   ngOnDestroy(): void {
-      this.subscription.unsubscribe();
+      // this.subscription.unsubscribe();
   }
 }

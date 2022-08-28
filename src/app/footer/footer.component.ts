9d0eaba98750a964db1d48a5d8a9a5e6c6fc46ce
Sorin Greu <37594039+SorinGreu23@users.noreply.github.com>
2022-08-28 22:37:48 +0300

    Updated footer and home page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  currentYear: string | undefined;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.getCurrentYear();
   }
 
+  getCurrentYear(): void {
+    this.currentYear = new Date().getFullYear().toString();
+  }
 }
